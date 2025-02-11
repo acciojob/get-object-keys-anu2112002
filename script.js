595ce6fc0103e95abfa731fdd3fc0429b9f99123
Anushri <112797477+anu2112002@users.noreply.github.com>
2025-02-11 11:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-// Create the student object
-const student = {
-  name: "John",
-};
-
-// Function to get all keys from the object
-function getKeys(obj) {
-  return Object.keys(obj);
+function Student(name){
+	this.name = name;
 }
 
-// Testing the function
-console.log(getKeys(student)); // Output: ["name"]
+function getKeys(obj){
+	return Object.keys(obj);
+}
 
-// Additional Tests
-const studentWithMultipleProperties = {
-  name: "Alice",
-  age: 22,
-  city: "Tokyo",
-};
-
-const studentWithOneProperty = {
-  name: "Bob",
-};
-
-// Test 1: Object with multiple properties
-console.log(getKeys(studentWithMultipleProperties)); // Output: ["name", "age", "city"]
-
-// Test 2: Object with only one property
-console.log(getKeys(studentWithOneProperty)); // Output: ["name"]
+console.log(getKeys(Student));
