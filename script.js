aa5b14961df8494457a4dc47af6741f6118bfec7
Anushri <112797477+anu2112002@users.noreply.github.com>
2025-02-11 09:19:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const student = {
+  name: "John",
+};
+
+function getKeys(obj) {
+  return Object.keys(obj);
+}
+
+console.log(getKeys(student)); 
+
+// const studentWithMultipleProperties = {
+//   name: "Alice",
+//   age: 22,
+//   city: "Tokyo",
+// };
+
+// const studentWithOneProperty = {
+//   name: "Bob",
+// };
+
+// console.log(getKeys(studentWithMultipleProperties)); 
+
+// console.log(getKeys(studentWithOneProperty));
